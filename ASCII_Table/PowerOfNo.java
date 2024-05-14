c57b4bdb2419a9c1d33ea329b238248a1b0258b2
Abhisek <abhiseksahoo7735@gmail.com>
2024-05-14 21:44:37 +0530

    Power of a number

---
+++ ASCII_Table/PowerOfNo.java
@@ -0,0 +1,12 @@
+//Power of a number 
+
+public class PowerOfNo {
+    public static void main(String[] args) {
+        int base = 2;
+        int exponent = 3;
+        
+        double result = Math.pow(base, exponent);
+        
+        System.out.println(base + " raised to the power of " + exponent + " is: " + result);
+    }
+}
