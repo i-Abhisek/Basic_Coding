d8b786afff133ac124cad1b3132b35434da7a8cd
Abhisek <abhiseksahoo7735@gmail.com>
2024-05-19 20:46:13 +0530

    finding the largest element of the array

--- Array/Find_Largest_Element.java
+++ Array/Find_Largest_Element.java
@@ -11,3 +11,31 @@ import java.util.*;
         System.out.println("The largest element is " + largest);
     }
  }
+
+
+ /*
+  * 
+  import java.util.Scanner;
+
+public class Find_Largest_Element
+{
+  public static void main(String args[])
+  {
+
+     int arr[] = {12, 13, 1, 10, 34, 10};
+
+     int max = arr[0];
+
+     for(int i=0; i<arr.length; i++)
+     {
+       if(max < arr[i])
+       {
+          max = arr[i];
+       }
+
+     }
+
+    System.out.print(max); 
+  }
+}
+  */
