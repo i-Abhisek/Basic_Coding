09aa8f5bd1f10d038d01ca7ad461a4b967501716
Abhisek <abhiseksahoo7735@gmail.com>
2024-05-20 08:53:37 +0530

    calculating the sum of all the elements of an array

---
+++ Array/Sum_Of_Array.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Sum_Of_Array {
+    public static void main(String[] args) {
+        int arr[] ={12,34,56,78,98,76,56,78,};
+        int sum =0;
+        for(int i=0;i<arr.length;i++) {
+            sum += arr[i];
+        }
+        System.out.println("sum is: " + sum);
+    }
+    
+}
