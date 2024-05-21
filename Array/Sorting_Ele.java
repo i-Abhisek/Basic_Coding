4423e35b903a86cbbe30b2255aa945c787bb097e
Abhisek <abhiseksahoo7735@gmail.com>
2024-05-21 08:33:05 +0530

    Sorting Elements In An Array

---
+++ Array/Sorting_Ele.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Sorting_Ele {
+    public static void main(String[] args) {
+        int numbers[] = {20,23,12,45,76,89,25,11,98,71};
+        Arrays.sort(numbers);
+    System.out.println("Sorted numbers is:" + Arrays.toString(numbers));
+    }
+    
+}
