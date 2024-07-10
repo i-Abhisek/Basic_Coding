49c7fcd2a0b920512297c04dbf63e75b8a85edc3
Abhisek <abhiseksahoo7735@gmail.com>
2024-07-11 00:20:53 +0530

    Sort the array in Java

---
+++ Array/SortArray.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class SortArray {
+    public static void main(String[] args) {
+        int arr[]={12,40,9,65,90,34,16,47,7,10};
+        int temp;
+          for(int i=0; i<arr.length; i++)
+          {
+            for(int j=i+1; j<arr.length; j++)
+            {
+                if(arr[i]>arr[j])
+                {
+                  temp=arr[i];
+                  arr[i]=arr[j];
+                  arr[j]=temp;
+                }
+            }
+          }
+          for(int i=0; i<arr.length; i++)
+          {
+            System.out.print(arr[i] + " ");
+          }
+    }
+    
+}
